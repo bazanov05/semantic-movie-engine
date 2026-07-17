f544cf095e4ff8f74c3807cb62f07931be5b8e06
Aliaksei Baranau <baralex05@gmail.com>
2026-07-17 15:04:29 +0200

    add SQL schema to create table in db

---
+++ src/db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE films (
+    film_id INT PRIMARY KEY,              
+    title TEXT NOT NULL,
+    genres JSONB,                         
+    keywords JSONB,                       
+    overview TEXT,                        
+    release_date DATE,                   
+    vote_average NUMERIC(3, 1) DEFAULT 0.0
+);
