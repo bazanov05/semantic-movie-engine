f2cddd0209e3fc80a70827a01e495dfffbc03d51
Aliaksei Baranau <baralex05@gmail.com>
2026-07-18 16:52:34 +0200

    wire up data unzipping, embedding generation, and database ingestion pipeline

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from src.data.cleaner import clean_data
-from src.data.loader import init_db, load_data_to_db
+from src.data.loader import (
+    init_db, 
+    load_data_to_db,
+    fetch_films_overviews,
+    update_film_embeddings
+)
 from src.db import connection
+from src.model.embedder import MovieEmbedder
 
 
 PATH_TO_CSV_DATA = "./src/data/tmdb_5000_movies.csv"
@@ -11,10 +17,25 @@ def main():
     connection.init_pool()
 
     clean_df = clean_data(file_path=PATH_TO_CSV_DATA)
+    model = MovieEmbedder()
 
     with connection.pool.connection() as conn:
         init_db(conn=conn, schema_path=PATH_TO_SQL_SCHEMA)
         load_data_to_db(conn=conn, df=clean_df)
+
+        # fetch film_ids and overviews from films table 
+        data = fetch_films_overviews(conn=conn)
+
+        # unzip data
+        ids = [item[0] for item in data]
+        overviews = [item[1] for item in data]
+
+        # generate vector representation for each overview
+        vectors = model.generate_embeddings(texts=overviews)
+        updates = list(zip(vectors, ids))   # zip data 
+
+        # insert vectors into the "embedding" cols based on provided ids 
+        update_film_embeddings(conn=conn, embedding_data=updates)
 
     connection.close_pool()
 
