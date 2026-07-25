09175dbaa91a03bf54df3da3d34a5f3856efe0d6
Aliaksei Baranau <baralex05@gmail.com>
2026-07-25 15:30:04 +0200

    extract one-time db setup into setup.py

---
+++ src/setup.py
@@ -0,0 +1,28 @@
+from src.data.cleaner import clean_data
+from src.data.loader import (
+    init_db, 
+    load_data_to_db
+)
+from src.db import connection
+
+
+PATH_TO_CSV_DATA = "./src/data/tmdb_5000_movies.csv"
+PATH_TO_SQL_SCHEMA = "./src/db/schema.sql"
+
+
+def setup():
+    connection.init_pool()
+
+    # clean data in .csv file
+    clean_df = clean_data(file_path=PATH_TO_CSV_DATA)
+
+    with connection.pool.connection() as conn:
+        # initialize db based on schema and load cleaned data to it
+        init_db(conn=conn, schema_path=PATH_TO_SQL_SCHEMA)
+        load_data_to_db(conn=conn, df=clean_df)
+
+    connection.close_pool()
+
+
+if __name__ == "__main__":
+    setup()
